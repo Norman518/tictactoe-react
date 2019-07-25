70afeecb25d5f0a3a2f824436f9e7b5301f38696
Vincent Teas <vincentteas@gmail.com>
2019-07-24 19:09:20 -0700

    Updates

---
+++ precache-manifest.c8cc0abeea0dee3159624d12e3e37fe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaaaa3a9eaa84003ccd18631b7c92acd",
+    "url": "/tictactoe-react/index.html"
+  },
+  {
+    "revision": "9103fe0de60617200e9a",
+    "url": "/tictactoe-react/static/css/main.8380678b.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tictactoe-react/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "9103fe0de60617200e9a",
+    "url": "/tictactoe-react/static/js/main.75f2160a.chunk.js"
+  },
+  {
+    "revision": "27fa6383a61f372e93fc",
+    "url": "/tictactoe-react/static/js/runtime~main.5413b239.js"
+  }
+]);
